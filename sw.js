6000951b1f7f56c1056499b48a8a964d6ed1a662
Skarvaan <155646872+Skarvaan@users.noreply.github.com>
2026-09-05 21:33:52 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
    Offline-Speicher mit (siehe speicher.js).
    ============================================================ */
 
-const VERSION = 'dew-v1';
+const VERSION = 'dew-v2';
 
 const DATEIEN = [
   './', './index.html', './spiel.html', './stil.css',
